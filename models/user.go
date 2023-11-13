287f286e7ba139b733fdbfa0da69702163eacd1a
Giwrgos Glinias <gergegl1999@gmail.com>
2023-11-13 20:47:51 +0200

    Fix Register

--- models/user.go
+++ models/user.go
@@ -1,10 +1,12 @@
 package models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type User struct {
-	Username string             `bson:"username"`
-	Email    string             `bson:"email"`
-	Password string             `bson:"password"`
-	ID       primitive.ObjectID `bson:"_id"`
+	ID       primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Username string             `bson:"username" json:"username"`
+	Email    string             `bson:"email" json:"email"`
+	Password string             `bson:"password" json:"-"`
 }
